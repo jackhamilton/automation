e6792974f9486ae48d7a1553fcd258ae4b44ab3f
Jack Hamilton <jackham800@gmail.com>
2023-11-07 18:44:15 -0500

    Update fxtwitter.sh

--- fxtwitter.sh
+++ fxtwitter.sh
@@ -1,8 +1,10 @@
 sleep 0.5
-pbpaste | perl -pe "s/(?<=\/\/)(?!fx)twitter(?=.com)/fxtwitter/" | xargs | pbcopy;
-#var2="$(pbpaste)"
+testStr="$(pbpaste)"
+if [[ "$testStr" =~ .*/twitter.* ]]; then
+pbpaste | perl -pe "s/(?<=\/\/)(?!fx)twitter(?=.com)/fxtwitter/" | perl -pe 'chomp' | pbcopy;
 #osascript -- - "$var2" <<'EOF'
 #  on run(argv)
 #    return display notification item 1 of argv
 #  end
 #EOF
+fi
