ada53f1eb9185e8488fa7d04fb9d43a67649f0af
Jack Hamilton <jack@vifive.ai>
2024-02-18 20:40:17 -0600

    Added quicktype

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,8 +1,10 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install npm
 brew install iterm
 brew install neovim
 brew install rbenv
 brew install gh
+npm install -g quicktype
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/powerline/fonts.git --depth=1
